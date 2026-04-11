bc23f530f5d8cd6ece1eb94384bd0e16f7d20cda
Al Mustarik <almustarik@gmail.com>
2026-04-11 13:13:58 +0600

    feat: implement getallsubject route and cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
-import express from "express";
+import express from 'express';
+import subjectsRouter from './routes/subjects';
+import cors from 'cors';
 
 const app = express();
 const PORT = 8000;
 
-app.use(express.json());
+// setup cors
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  }),
+);
 
-app.get("/", (req, res) => {
-  res.send("Backend server is running!");
+app.use(express.json());
+app.use('/api/subjects', subjectsRouter);
+
+app.get('/', (req, res) => {
+  res.send('Backend server is running!');
 });
 
 app.listen(PORT, () => {
